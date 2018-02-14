21f3f28cc55d1c3a4a201065fb152bd36971c65c
phil <phil@home.de>
2018-02-14 16:26:46 +0100

    Added a function that counts the biggest numerical difference in an array

--- php_functions.php
+++ php_functions.php
@@ -1,4 +1,6 @@
 <?php 
+/* This one checks an input for its type and outputs either that it is an int or not.
+*/
 
 function TypeCheck($i) 
 {
@@ -14,4 +16,40 @@ function TypeCheck($i)
 	}
 }
 
-Typecheck();
+
+
+/*
+This class takes N, showing the number of integers and $a which is an array of integers. it then prints out the maximum difference between the integers (between the biggest and the smallest one).
+*/
+    class Difference{
+    private $elements=array();
+    public $maximumDifference;
+
+
+
+function __construct($a) 
+{        
+    $this->elements = $a;            
+}
+
+function ComputeDifference()
+{   
+    $elements = $this-> elements;
+    $length = count($elements) - 1;
+    
+    sort ($elements);
+    
+    $maximumDifference = $elements[$length] - $elements[0];
+  
+    print_r( $maximumDifference );
+    
+}
+
+} 
+     
+
+$N = intval(fgets(STDIN));
+$a = array_map('intval', explode(' ', fgets(STDIN)));
+$d = new Difference($a);
+$d-> ComputeDifference();
+print ($d->maximumDifference);
